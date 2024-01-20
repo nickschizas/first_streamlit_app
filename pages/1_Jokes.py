2876a2b1872abbe6c85e8f25c63b8f48ecbdaa20
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-01-20 20:53:29 +0200

    Create 1_Jokes.py

---
+++ pages/1_Jokes.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import random
+
+#search for questionmark
+def contains_questionmark(text):
+    return True if '?' in text else False
+
+#import the data
+jokes = pd.read_csv('all_jokes.csv', sep = ';', index_col=False)
+
+st.header('Joke?', divider = 'gray')
+if st.button('Hit me'):
+    index = random.randint(0, len(jokes)-1)
+    joke = jokes.iloc[index,0]
+    if contains_questionmark(joke):
+        question = joke.split('?')[0]+'?'
+        answer = joke.split('?')[1]
+        st.write(question)
+        st.write(answer)
+    else:
+        st.write(joke)
+
+
+new_joke = st.text_input('Want to contribute?', placeholder = 'Write you joke here')
+
+#read the template df for new jokes
+new_jokes = pd.read_csv('new_jokes.csv', sep = ';', index_col=False)
+if new_joke and new_joke not in jokes[['Joke']] and new_joke not in new_jokes:
+   new_jokes.loc[len(new_jokes)] = new_joke
+
+st.write(new_jokes)
+new_jokes.to_csv('new_jokes.csv', sep=';', index = False)
