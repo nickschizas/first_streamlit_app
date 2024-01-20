4a92573a9e6ddc5cf01ae0a5afebdf2216ed5142
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-01-20 19:10:47 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-#My first streamlit deployed app
-
 import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import random
 
-#Making the text of the st.code
+#search for questionmark
+def contains_questionmark(text):
+    return True if '?' in text else False
+
 code = '''class Creator:
 	def __init__(self, first_name, last_name):
 		self.first_name = Nick
     		self.last_name = Schizas''' 
 
-#Visible elements of the page
-st.header('Hello all, this is my first deployed app', divider = 'gray')
-st.code(code, language='python')
+#import the data
+jokes = pd.read_csv('all_jokes.csv', sep = ';', index_col=False)
 
-name = st.text_input('', placeholder = 'Name')
-if st.button('Say hi'):
-	if name:
-		st.write('Hi, '+name+'. Good to see you !')
-	else:
-		st.write('*Please enter your name above first*')
+#Page visible elements
+with st.sidebar:
+    selected = option_menu(
+        menu_title = None,
+        options = ['Hello', 'Joke?']
+    )
+if selected == 'Hello':
+    st.header('Hello :wave:', divider = 'gray')
+    st.code(code, language='python')
+    name = st.text_input('', placeholder = 'Name')
+    if st.button('Say hi'):
+        if name:
+            st.write('Greetings, '+name+'. Good to see you !')
+        else:
+            st.write('*Please enter your name above first*')
+if selected == 'Joke?':
+    st.header('Joke?', divider = 'gray')
+    if st.button('Hit me'):
+        index = random.randint(0, len(jokes)-1)
+        joke = jokes.iloc[index,0]
+        if contains_questionmark(joke):
+            question = joke.split('?')[0]+'?'
+            answer = joke.split('?')[1]
+            st.write(question)
+            st.write(answer)
+        else:
+            st.write(joke)
