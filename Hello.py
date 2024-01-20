d0946f2947e8c24c088cf8d2bbf9d91204cee8b4
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-01-21 00:27:59 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,7 +13,8 @@ st.header('Hello there! :wave:', divider = 'gray')
 st.code(code, language='python')
 
 name = st.text_input('Say hi', placeholder = 'Enter your name here...')
-st.write('Greetings, '+name+'! Good to see you !')
+if name:
+	st.write('Greetings, '+name+'! Good to see you !')
 
 names = pd.read_csv('names.csv', sep = ';')
 names.loc[len(names)] = [datetime.now().date().strftime("%d/%m/%Y"), name]
