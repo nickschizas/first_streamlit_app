13db6f984aae6593a0cc6893907fa0ed242cb71c
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-02-11 00:58:31 +0200

    Update 1_Jokes.py

--- pages/1_Jokes.py
+++ pages/1_Jokes.py
@@ -10,7 +10,7 @@ def contains_questionmark(text):
 jokes = pd.read_csv('all_jokes.csv', sep = ';', index_col=False)
 
 st.header('May I tell you a joke?', divider = 'gray')
-if st.button('yeap'):
+if st.button('Hit me!'):
     index = random.randint(0, len(jokes)-1)
     joke = jokes.iloc[index,0]
     if contains_questionmark(joke):
