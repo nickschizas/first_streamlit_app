2f9cb0a9836e0b859c0a78a18a0ccdc01523eaea
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-02-11 01:13:52 +0200

    Update 1_Jokes.py

--- pages/1_Jokes.py
+++ pages/1_Jokes.py
@@ -22,9 +22,9 @@ if st.button('Hit me!'):
         st.write(joke)
 
 st.divider()
-#def new_joke(joke):
- #   new_joke_df = pd.DataFrame({'Joke' : joke})
-  #  new_joke_df.to_csv() 
+def new_joke(joke):
+    new_joke_df = pd.DataFrame({'Joke' : joke})
+    new_joke_df.to_csv('new_joke.csv', sep=';') 
 
-st.text_area(label='Want to contribute?', placeholder='write your joke here', key='new_joke')
+st.text_area(label='Want to contribute?', placeholder='write your joke here', key='new_joke', on_change=new_joke(st.session_state.new_joke)
 st.write(st.session_state.new_joke)
