3771c7955fcdb9816e8667c329174dc5631ac056
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-02-11 01:02:11 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-st.set_page_config(page_title='MyFirstApp', layout='centered')
+st.set_page_config(page_title='Hello', layout='centered')
 
 code = '''class Me:
 	def __init__(self, first_name, last_name):
